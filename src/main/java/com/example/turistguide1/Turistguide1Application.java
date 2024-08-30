7f3d69dafe08404142fec20964a62cb464f2bf69
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-08-30 15:40:34 +0200

    first commit

---
+++ src/main/java/com/example/turistguide1/Turistguide1Application.java
@@ -0,0 +1,13 @@
+package com.example.turistguide1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Turistguide1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Turistguide1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/turistguide1/Turistguide1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turistguide1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turistguide1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
